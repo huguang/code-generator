0a07654a2252b6d9752e5977c1a05b9891ef8f88
huguang <m13810122935@163.com>
2018-11-11 01:14:44 +0800

    project init

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mvnw clean package exec:java -Dexec.mainClass="me.hgcn.tools.codegen.Main"
---
+++ src/main/java/me/hgcn/tools/codegen/Main.java
@@ -0,0 +1,9 @@
+package me.hgcn.tools.codegen;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("code gen start");
+        System.out.println("code gen complete");
+    }
+}
